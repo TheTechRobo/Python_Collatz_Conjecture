7435f57b4ceaeb7aab48a10f2e73a54b5ff8f4b3
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-05-17 09:36:42 -0400

    Added Option to Print Numbers, Plus...
    Slightly Smaller Filesize, Better Variable Management

--- Collatz_Conjecture.py
+++ Collatz_Conjecture.py
@@ -5,16 +5,23 @@ from time import clock
 it_count = 0
 n = 0
 
+p = input("Would you like to print all numbers? The downside is that it may slow program speed.\nType Y/N: ")
+p = p.lower()
+if p == "y":
+    p = True
+else:
+    p = False
+
 def Select_Number():
-    print ("")
+    print()
     sn = int(input("Select the number you wish to calculate? "))
     if (sn == 0):
-        print ("This number is invalid.")
-        print ()
-        print ()
-        Select_Number();
+        print("This number is invalid.")
+        print()
+        print()
+        Select_Number()
     else:
-        Calculate(sn);
+        Calculate(sn)
 
 def Calculate(n):
     global it_count
@@ -24,18 +31,19 @@ def Calculate(n):
     while n != 1:
         if (n % 2):
             n = (n*3+1)
-            #print (n) #Prints All Numbers (Slows Program Speed)
+            if p:
+                print(n) #Prints All Numbers (Slows Program Speed)
             it_count += 1
         else:
             n = (n//2)
-            #print (n) #Prints All Numbers (Slows Program Speed)
+            if p:
+                print(n) #Prints All Numbers (Slows Program Speed)
             it_count += 1
 
     end = clock()
-    print ("The number has reached " + str(n) + " with only " + str(it_count) + " iterations! (Time taken: " + format(end-start, ".10f") + " seconds.)")
+    lolz = format(end-start, ".10f")
+    print("The number has reached %s with only %s iterations! (Time taken:  %s seconds.)" % (str(n), str(it_count), lolz))
     print()
-    Select_Number();
+    Select_Number()
 
-Select_Number();
-
-
+Select_Number()
